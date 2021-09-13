b8b84e3f38c440f2a7e91a1404fec19f7896ad88
dantemogni <dantejoaquin.mogni@gmail.com>
2021-09-13 16:59:15 -0300

    jQuery for modals

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,13 @@
 /* scripts */
+
+$('#modalDelete').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var id = button.data('id'); // Extract info from data-* attributes
+    var name = button.data('name');
+    var surname = button.data('surname');
+    var modal = $(this);
+
+    modal.find('.modal-title').text('Borrar empleado #' + id);
+    modal.find('#delete-name').text(name + ' ' + surname);
+    modal.find('#del-btn').attr("href", '/destroy/'+id);
+  })
